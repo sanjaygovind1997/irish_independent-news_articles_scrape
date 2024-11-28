3c67176f6b1a3675e25a9d9d9ed0b70ea579c6f9
sanjay <sanjaygovind1997@gmail.com>
2024-11-29 05:25:53 +0530

    Handled pagination; Added asynchronous multiple keyword search

--- headers.py
+++ headers.py
@@ -14,6 +14,23 @@ listing_headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
 }
 
+listing_graphql_headers = {
+    'accept': '*/*',
+    'accept-language': 'en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,ml;q=0.6',
+    'apollographql-client-name': 'article-list-v2',
+    'apollographql-client-version': '0.0.1245-indo',
+    'priority': 'u=1, i',
+    'referer': 'https://www.independent.ie/search?keyword=substance+abuse&daterange=all&datestart=&dateend=',
+    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+    'x-client-name': 'article-list-v2-indo',
+}
+
 artilce_page_headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-GB,en;q=0.9',
--- scraper.py
+++ scraper.py
@@ -5,11 +5,74 @@ import json
 import os
 from pathlib import Path
 from nested_lookup import nested_lookup
+from asyncio import create_task, gather, run
+from unidecode import unidecode
 
-from headers import listing_headers, artilce_page_headers
-from regex import articles_list_regex, html_tags_pattern, json_regex
+from headers import listing_headers, artilce_page_headers, listing_graphql_headers
+from regex_patterns import articles_list_regex, html_tags_pattern, json_regex
 from xpaths import article_json_xpath, article_list_xpath
 
+async def search_keyword(key_word, page_no, end_cursor=None):
+
+    print(f"\nSearching for article URLs related to : {key_word}. Page Number : {page_no}\n")
+    key_word_formatted = key_word.replace(' ', '+').lower()
+    if page_no == 1:
+        url = f'https://www.independent.ie/search?keyword={key_word_formatted}&daterange=all&datestart=&dateend='
+
+        response = requests.get(url, headers=listing_headers)
+        print(f"\n{key_word} : Page - {page_no} response statsus : {response.status_code}\n")
+        parser = html.fromstring(response.text)
+        articles_list_raw = parser.xpath(article_list_xpath)
+        articles_list_raw = articles_list_raw[0] if articles_list_raw else None
+
+        articles_list = re.search(articles_list_regex, articles_list_raw)
+        articles_list = articles_list.group(1)
+        articles_list = json.loads(articles_list)
+    else:
+        url = 'https://www.independent.ie/graphql-blue-mhie'
+        params = {
+            'variables': '{"brand":"independent.ie","count":50,"search":"' + key_word.lower() + '","publishedAfter":"","publishedBefore":"",'
+            '"sections":[],"ordering":"MOST_RECENT","sourcesetCroppingInput":{"resizeMode":"CENTER_CROP","cropsMode":"LANDSCAPE","fallbackResizeMode":"SMART_CROP",'
+            '"sizes":[{"key":"xsmall","width":120,"height":80},{"key":"small","width":160,"height":107},{"key":"smallMobile","width":240,"height":160},'
+            '{"key":"medium","width":320,"height":213},{"key":"large","width":640,"height":427},{"key":"xlarge","width":960,"height":640},{"key":"xxlarge","width":1280,'
+            '"height":853}]},"after":"' + end_cursor + '"}',
+            'operationName': 'webv2_SearchArticlesConnection_indo_3_2',
+            'persisted': 'true',
+            'extensions': '{"persistedQuery":{"version":1,"sha256Hash":"43e500c22cefdeeb9dd4d386a63bcff60485f16d48e6419d65365f8e77e844b5"}}',
+            }
+        print(f"\nparams\n{params}")
+        headers = listing_graphql_headers
+        headers['referer'] = f'https://www.independent.ie/search?keyword={key_word_formatted}&daterange=all&datestart=&dateend='
+
+        response = requests.get(url, params=params, headers=headers)
+        print(f"\n{key_word} : Page - {page_no} response statsus : {response.status_code}\n")
+        articles_list = response.json()
+
+    print(url)
+    data = nested_lookup('data', articles_list)
+
+    total_count = data[0].get('search', {}).get('totalCount')
+    print(f"\nTotals Articles listed on the website : {total_count}\n")
+    page_info = data[0].get('search', {}).get('pageInfo', {})
+    end_cursor, has_next_page = page_info.get('endCursor'), page_info.get('hasNextPage')
+    articles = data[0].get('search').get('edges',[])
+
+    article_urls = []
+    for article in articles:
+        article_url = 'https://www.independent.ie/' + article.get('node',{}).get('relativeUrl')
+        article_urls.append(article_url)
+    print(f"\nArticle URLs ({key_word}): \n", article_urls)
+
+    print("\n Articles len : \n", len(article_urls))
+
+    # for url in article_urls:
+    #     get_article_data(url)
+    #     # break
+
+    if has_next_page and page_no != 2:
+        print(f"\nNext page exists! Getting next 50 URLs for {key_word} ...\n")
+        page_no += 1
+        await search_keyword(key_word, page_no, end_cursor)
 
 def get_article_data(article_url):
     """
@@ -26,9 +89,9 @@ def get_article_data(article_url):
 
     article = nested_lookup('article', article_json)[0] if nested_lookup('article', article_json) else {}
 
-    title = article.get("title")
+    title = unidecode(article.get("title"))
     url = article.get("url", response.url)
-    author = article.get("author")
+    author = unidecode(article.get("author"))
     authors = article.get("authors")
     published_date = article.get("publishedAt")
     type_ = article.get("type")
@@ -55,7 +118,8 @@ def clean_paragraphs(paragraph_elems):
     Extracts article's paragraph texts(contained in <p> tags), strips any html tags present in it.
     """
     paragraphs = [re.sub(html_tags_pattern, '', elem['p']) for elem in paragraph_elems if elem.get('p')]
-    return ' '.join(paragraphs)
+    combined_paragraphs = ' '.join(paragraphs)
+    return unidecode(combined_paragraphs)
 
 def write_to_file(data):
     """
@@ -80,42 +144,16 @@ def write_to_file(data):
             json.dump(articles_list, file)
 
 
-def main():
+async def main():
     """
     Add the necessary key words for searching to the key_words list.
     Function will search for the articles and extract data
     """
 
-    key_words = ["Substance Abuse"]
+    key_words = ["Substance Abuse", "murder"]
 
-    for key_word in key_words:
-        key_word = key_word.replace(' ', '+').lower()
-        url = f'https://www.independent.ie/search?keyword={key_word}&daterange=all&datestart=&dateend='
+    tasks = [ create_task(search_keyword(key_word, 1)) for key_word in key_words]
+    await gather(*tasks)
 
-        response = requests.get(url, headers=listing_headers)
-        parser = html.fromstring(response.text)
-        articles_list_raw = parser.xpath(article_list_xpath)
-        articles_list_raw = articles_list_raw[0] if articles_list_raw else None
 
-        articles_list = re.search(articles_list_regex, articles_list_raw)
-        articles_list = articles_list.group(1)
-        articles_list = json.loads(articles_list)
-
-        data = nested_lookup('data', articles_list)
-
-        total_count = data[0].get('search', {}).get('totalCount')
-        page_info = data[0].get('search', {}).get('pageInfo', {})
-        end_cursor, has_next_page = page_info.get('endCursor'), page_info.get('hasNextPage')
-        articles = data[0].get('search').get('edges',[])
-
-        article_urls = []
-        for article in articles:
-            article_url = 'https://www.independent.ie/' + article.get('node',{}).get('relativeUrl')
-            article_urls.append(article_url)
-        print("Article URLs : ", article_urls)
-
-        for url in article_urls:
-            get_article_data(url)
-            # break
-
-main()
+run(main())
